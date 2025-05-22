8308673ec75eaf3f4d81993b01e5349ba6d087a5
msonsite <sanderdelobel@outlook.com>
2025-05-22 19:11:52 +0200

    update

--- pages/2_Genereer Hoogteprofiel.py
+++ pages/2_Genereer Hoogteprofiel.py
@@ -90,7 +90,7 @@ if uploaded_file is not None:
 
         window_length = st.slider(
             "Hoe vloeiend de lijn is (meer = gladder)",
-            5, 301, 61, step=2,
+            5, 801, 61, step=2,
             help="Hoe sterk het hoogteprofiel wordt gladgestreken. Grotere waarde betekent een zachtere, vloeiendere lijn."
         )
 
